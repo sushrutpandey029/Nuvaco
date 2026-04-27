665fe5b05b7e4e7d72a6fb872e555c71e8ee5a5e
Sushrut Pandey <102680316+sushrutpandey029@users.noreply.github.com>
2026-04-27 23:40:17 +0530

    project setup and admin setup

---
+++ utils/mailer.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+export const sendMail = async ({ to, subject, text, html }) => {
+    try {
+        await transporter.sendMail({
+            from: `"Nuvaco" <${process.env.EMAIL_USER}>`,
+            to,
+            subject,
+            text,  
+            html,   
+        });
+
+        console.log("Email sent");
+    } catch (error) {
+        console.error("Email error", error);
+        throw error;
+    }
+};
