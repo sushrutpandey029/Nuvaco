84984bd162c3e424512cee95b78265be14ba3f14
Sushrut Pandey <102680316+sushrutpandey029@users.noreply.github.com>
2026-04-27 23:40:17 +0530

    project setup and admin setup

--- server.js
+++ server.js
@@ -1,16 +1,50 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import hbs from "hbs";
+
+import { Db_connection, sequelize } from "./DBConnection/mysqlconnetion.js";
+import adminrouter from "./Route/adminRoutes.js";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.port || 5050;
+const PORT = process.env.PORT || 5050;
 
-app.get("/",(req,res)=>{
-    res.send("yes done");
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.listen(port,(req,res)=>{
-    console.log(`http://localhost:${port}`)
-})
 
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "view"));
+
+hbs.registerPartials(path.join(__dirname, "view", "partials"));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.use("/", adminrouter);
+
+
+const startServer = async () => {
+  try {
+    await Db_connection();
+
+    await sequelize.sync();
+
+    app.listen(PORT, () => {
+      console.log(`Server running: http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Server not started due to DB error");
+    process.exit(1);
+  }
+};
+
+startServer();
