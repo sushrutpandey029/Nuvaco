962d0ee8d7b189db9245a9dafd51d4710bef2ebd
Zeba Shafi <zebaperween 9340@gmail.com>
2026-04-28 12:53:01 +0530

    delaer done

--- Route/adminRoutes.js
+++ Route/adminRoutes.js
@@ -1,11 +1,15 @@
 import express from "express"
 
-import {AdminRegister,adminloginview,adminlogin} from "../Controller/AdminController/Admin.js"
+import {AdminRegister,adminloginview,adminlogin,registerDealer} from "../Controller/AdminController/Admin.js"
 
 const adminrouter = express.Router();
 
 adminrouter.post("/adminsigup",AdminRegister);
 adminrouter.get("/",adminloginview);
 adminrouter.post("/adminsigin",adminlogin);
+
+// dealer Registration routes
+
+adminrouter.post("/regi-delars",registerDealer);
 
 export default adminrouter;
